3ae4f18768e1998c6873a4f53f2d31bd28171b06
msheraznadeem <sheraz_nadeem04@yahoo.com>
2019-02-18 02:57:57 +0500

    Implemented network data source which is responsible to fetch data from network using GitHubApiService. Interface GitHubNetworkDataSource is implemented by its corresponding implementation class GitHubNetworkDataSourceImpl.

---
+++ list_repos/app/src/main/java/com/sheraz/listrepos/data/network/GitHubNetworkDataSource.kt
@@ -0,0 +1,11 @@
+package com.sheraz.listrepos.data.network
+
+import androidx.lifecycle.LiveData
+import com.sheraz.listrepos.data.db.entity.GitHubRepoEntity
+
+interface GitHubNetworkDataSource {
+
+    val downloadedGitHubRepoList: LiveData<List<GitHubRepoEntity>>
+    suspend fun fetchGitHubRepos(page: Int, per_page: Int = 10)
+
+}
---
+++ list_repos/app/src/main/java/com/sheraz/listrepos/data/network/GitHubNetworkDataSourceImpl.kt
@@ -0,0 +1,57 @@
+package com.sheraz.listrepos.data.network
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.sheraz.listrepos.data.db.entity.GitHubRepoEntity
+import com.sheraz.listrepos.utils.Logger
+
+class GitHubNetworkDataSourceImpl(
+    private val gitHubApiService: GitHubApiService
+) : GitHubNetworkDataSource {
+
+
+    init {
+        Logger.d(TAG, "init(): ")
+    }
+
+    /**
+     * Encapsulating Mutable value so that it can only be changed within the "fetchGitHubRepos()" method
+     */
+    private val _downloadedGitHubRepoList = MutableLiveData<List<GitHubRepoEntity>>()
+    override val downloadedGitHubRepoList: LiveData<List<GitHubRepoEntity>>
+        get() = _downloadedGitHubRepoList
+
+
+    override suspend fun fetchGitHubRepos(page: Int, per_page: Int) {
+
+        Logger.d(TAG, "fetchGitHubRepos(): page: $page, per_page: $per_page")
+
+        try {
+
+            val fetchedReposWithPage = gitHubApiService
+                .getReposWithPageAsync(page, per_page)
+                .await()
+
+            Logger.v(TAG, "fetchGitHubRepos(): fetchedReposWithPage: ${fetchedReposWithPage.size}")
+            Logger.v(TAG, "fetchGitHubRepos(): fetchedReposWithPage: $fetchedReposWithPage")
+            // MutableLiveData.postValue must be called from background thread,
+            // that is why this function is a "suspend" function which will
+            // be called from a coroutine.
+            // Finally, postValue will post a task on "main thread" to set the
+            // given value
+            _downloadedGitHubRepoList.postValue(fetchedReposWithPage)
+
+        } catch (e: Exception) {
+            Logger.e(TAG, "fetchGitHubRepos(): Exception occurred, Error => ", e)
+        }
+    }
+
+
+    /**
+     * Companion object, common to all instances of this class
+     * Similar to static fields in Java
+     */
+    companion object {
+        private val TAG: String = GitHubNetworkDataSourceImpl::class.java.simpleName
+    }
+}
