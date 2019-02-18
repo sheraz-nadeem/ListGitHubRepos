616e59b7050d394e6fa21048fd855bfcbde0f333
msheraznadeem <sheraz_nadeem04@yahoo.com>
2019-02-18 06:21:22 +0500

    Fixed issue with Room library not being able to generate implementation due to a wrong constructor parameter named "private". Also made String values to be optional with default values as empty string.

--- list_repos/app/src/main/java/com/sheraz/listrepos/data/db/entity/GitHubRepoEntity.kt
+++ list_repos/app/src/main/java/com/sheraz/listrepos/data/db/entity/GitHubRepoEntity.kt
@@ -1,5 +1,6 @@
 package com.sheraz.listrepos.data.db.entity
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
@@ -13,32 +14,38 @@ data class GitHubRepoEntity(
     @PrimaryKey(autoGenerate = true)
     val rowId: Int,
 
-    @ColumnInfo(name = "repoId")
+    @ColumnInfo(name = "repo_id")
     @SerializedName("id")
     val repoId: Int,
 
-    @ColumnInfo(name = "fullName")
+    @ColumnInfo(name = "full_name")
     @SerializedName("full_name")
+    @NonNull
     val fullName: String,
 
     @ColumnInfo(name = "name")
+    @NonNull
     val name: String,
 
-    @ColumnInfo(name = "private")
-    val private: Boolean,
+    @ColumnInfo(name = "is_repo_private")
+    @SerializedName("private")
+    val isRepoPrivate: Boolean,
 
     @Embedded(prefix = "owner_")
+    @NonNull
     val owner: Owner,
 
     @ColumnInfo(name = "html_url")
     @SerializedName("html_url")
+    @NonNull
     val htmlUrl: String,
 
     @ColumnInfo(name = "url")
+    @NonNull
     val url: String,
 
     @ColumnInfo(name = "description")
-    val description: Any,
+    val description: String? = "",
 
     @ColumnInfo(name = "fork")
     val fork: Boolean,
@@ -52,70 +59,70 @@ data class GitHubRepoEntity(
 
     @ColumnInfo(name = "archive_url")
     @SerializedName("archive_url")
-    val archiveUrl: String,
+    val archiveUrl: String? = "",
 
     @ColumnInfo(name = "archived")
     val archived: Boolean,
 
     @ColumnInfo(name = "assignees_url")
     @SerializedName("assignees_url")
-    val assigneesUrl: String,
+    val assigneesUrl: String? = "",
 
     @ColumnInfo(name = "blobs_url")
     @SerializedName("blobs_url")
-    val blobsUrl: String,
+    val blobsUrl: String? = "",
 
     @ColumnInfo(name = "branches_url")
     @SerializedName("branches_url")
-    val branchesUrl: String,
+    val branchesUrl: String? = "",
 
     @ColumnInfo(name = "clone_url")
     @SerializedName("clone_url")
-    val cloneUrl: String,
+    val cloneUrl: String? = "",
 
     @ColumnInfo(name = "collaborators_url")
     @SerializedName("collaborators_url")
-    val collaboratorsUrl: String,
+    val collaboratorsUrl: String? = "",
 
     @ColumnInfo(name = "comments_url")
     @SerializedName("comments_url")
-    val commentsUrl: String,
+    val commentsUrl: String? = "",
 
     @ColumnInfo(name = "commits_url")
     @SerializedName("commits_url")
-    val commitsUrl: String,
+    val commitsUrl: String? = "",
 
     @ColumnInfo(name = "compare_url")
     @SerializedName("compare_url")
-    val compareUrl: String,
+    val compareUrl: String? = "",
 
     @ColumnInfo(name = "contents_url")
     @SerializedName("contents_url")
-    val contentsUrl: String,
+    val contentsUrl: String? = "",
 
     @ColumnInfo(name = "contributors_url")
     @SerializedName("contributors_url")
-    val contributorsUrl: String,
+    val contributorsUrl: String? = "",
 
     @ColumnInfo(name = "created_at")
     @SerializedName("created_at")
-    val createdAt: String,
+    val createdAt: String? = "",
 
     @ColumnInfo(name = "default_branch")
     @SerializedName("default_branch")
-    val defaultBranch: String,
+    val defaultBranch: String? = "",
 
     @ColumnInfo(name = "deployments_url")
     @SerializedName("deployments_url")
-    val deploymentsUrl: String,
+    val deploymentsUrl: String? = "",
 
     @ColumnInfo(name = "downloads_url")
     @SerializedName("downloads_url")
-    val downloadsUrl: String,
+    val downloadsUrl: String? = "",
 
     @ColumnInfo(name = "events_url")
     @SerializedName("events_url")
-    val eventsUrl: String,
+    val eventsUrl: String? = "",
 
     @ColumnInfo(name = "forks")
     val forks: Int,
@@ -126,23 +133,23 @@ data class GitHubRepoEntity(
 
     @ColumnInfo(name = "forks_url")
     @SerializedName("forks_url")
-    val forksUrl: String,
+    val forksUrl: String? = "",
 
     @ColumnInfo(name = "git_commits_url")
     @SerializedName("git_commits_url")
-    val gitCommitsUrl: String,
+    val gitCommitsUrl: String? = "",
 
     @ColumnInfo(name = "git_refs_url")
     @SerializedName("git_refs_url")
-    val gitRefsUrl: String,
+    val gitRefsUrl: String? = "",
 
     @ColumnInfo(name = "git_tags_url")
     @SerializedName("git_tags_url")
-    val gitTagsUrl: String,
+    val gitTagsUrl: String? = "",
 
     @ColumnInfo(name = "git_url")
     @SerializedName("git_url")
-    val gitUrl: String,
+    val gitUrl: String? = "",
 
     @ColumnInfo(name = "has_downloads")
     @SerializedName("has_downloads")
@@ -165,58 +172,58 @@ data class GitHubRepoEntity(
     val hasWiki: Boolean,
 
     @ColumnInfo(name = "homepage")
-    val homepage: Any,
+    val homepage: String? = "",
 
     @ColumnInfo(name = "hooks_url")
     @SerializedName("hooks_url")
-    val hooksUrl: String,
+    val hooksUrl: String? = "",
 
     @ColumnInfo(name = "issue_comment_url")
     @SerializedName("issue_comment_url")
-    val issueCommentUrl: String,
+    val issueCommentUrl: String? = "",
 
     @ColumnInfo(name = "issue_events_url")
     @SerializedName("issue_events_url")
-    val issueEventsUrl: String,
+    val issueEventsUrl: String? = "",
 
     @ColumnInfo(name = "issues_url")
     @SerializedName("issues_url")
-    val issuesUrl: String,
+    val issuesUrl: String? = "",
 
     @ColumnInfo(name = "keys_url")
     @SerializedName("keys_url")
-    val keysUrl: String,
+    val keysUrl: String? = "",
 
     @ColumnInfo(name = "labels_url")
     @SerializedName("labels_url")
-    val labelsUrl: String,
+    val labelsUrl: String? = "",
 
     @ColumnInfo(name = "language")
-    val language: String,
+    val language: String? = "",
 
     @ColumnInfo(name = "languages_url")
     @SerializedName("languages_url")
-    val languagesUrl: String,
+    val languagesUrl: String? = "",
 
     @ColumnInfo(name = "merges_url")
     @SerializedName("merges_url")
-    val mergesUrl: String,
+    val mergesUrl: String? = "",
 
     @ColumnInfo(name = "milestones_url")
     @SerializedName("milestones_url")
-    val milestonesUrl: String,
+    val milestonesUrl: String? = "",
 
     @ColumnInfo(name = "mirror_url")
     @SerializedName("mirror_url")
-    val mirrorUrl: Any,
+    val mirrorUrl: String? = "",
 
     @ColumnInfo(name = "node_id")
     @SerializedName("node_id")
-    val nodeId: String,
+    val nodeId: String? = "",
 
     @ColumnInfo(name = "notifications_url")
     @SerializedName("notifications_url")
-    val notificationsUrl: String,
+    val notificationsUrl: String? = "",
 
     @ColumnInfo(name = "open_issues")
     @SerializedName("open_issues")
@@ -228,22 +235,22 @@ data class GitHubRepoEntity(
 
     @ColumnInfo(name = "pulls_url")
     @SerializedName("pulls_url")
-    val pullsUrl: String,
+    val pullsUrl: String? = "",
 
     @ColumnInfo(name = "pushed_at")
     @SerializedName("pushed_at")
-    val pushedAt: String,
+    val pushedAt: String? = "",
 
     @ColumnInfo(name = "releases_url")
     @SerializedName("releases_url")
-    val releasesUrl: String,
+    val releasesUrl: String? = "",
 
     @ColumnInfo(name = "size")
     val size: Int,
 
     @ColumnInfo(name = "ssh_url")
     @SerializedName("ssh_url")
-    val sshUrl: String,
+    val sshUrl: String? = "",
 
     @ColumnInfo(name = "stargazers_count")
     @SerializedName("stargazers_count")
@@ -251,38 +258,38 @@ data class GitHubRepoEntity(
 
     @ColumnInfo(name = "stargazers_url")
     @SerializedName("stargazers_url")
-    val stargazersUrl: String,
+    val stargazersUrl: String? = "",
 
     @ColumnInfo(name = "statuses_url")
     @SerializedName("statuses_url")
-    val statusesUrl: String,
+    val statusesUrl: String? = "",
 
     @ColumnInfo(name = "subscribers_url")
     @SerializedName("subscribers_url")
-    val subscribersUrl: String,
+    val subscribersUrl: String? = "",
 
     @ColumnInfo(name = "subscription_url")
     @SerializedName("subscription_url")
-    val subscriptionUrl: String,
+    val subscriptionUrl: String? = "",
 
     @ColumnInfo(name = "svn_url")
     @SerializedName("svn_url")
-    val svnUrl: String,
+    val svnUrl: String? = "",
 
     @ColumnInfo(name = "tags_url")
     @SerializedName("tags_url")
-    val tagsUrl: String,
+    val tagsUrl: String? = "",
 
     @ColumnInfo(name = "teams_url")
     @SerializedName("teams_url")
-    val teamsUrl: String,
+    val teamsUrl: String? = "",
 
     @ColumnInfo(name = "trees_url")
     @SerializedName("trees_url")
-    val treesUrl: String,
+    val treesUrl: String? = "",
 
     @ColumnInfo(name = "updated_at")
     @SerializedName("updated_at")
-    val updatedAt: String
+    val updatedAt: String? = ""
 
 )
--- list_repos/app/src/main/java/com/sheraz/listrepos/data/db/entity/Owner.kt
+++ list_repos/app/src/main/java/com/sheraz/listrepos/data/db/entity/Owner.kt
@@ -1,5 +1,6 @@
 package com.sheraz.listrepos.data.db.entity
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import com.google.gson.annotations.SerializedName
 
@@ -9,68 +10,70 @@ data class Owner(
     val id: Int,
 
     @ColumnInfo(name = "owner_type")
-    val type: String,
+    val type: String? = "",
 
     @ColumnInfo(name = "owner_url")
+    @NonNull
     val url: String,
 
     @ColumnInfo(name = "owner_html_url")
     @SerializedName("html_url")
+    @NonNull
     val htmlUrl: String,
 
     @ColumnInfo(name = "owner_avatar_url")
     @SerializedName("avatar_url")
-    val avatarUrl: String,
+    val avatarUrl: String? = "",
 
     @ColumnInfo(name = "owner_login")
-    val login: String,
+    val login: String? = "",
 
     @ColumnInfo(name = "owner_repos_url")
     @SerializedName("repos_url")
-    val reposUrl: String,
+    val reposUrl: String? = "",
 
     @ColumnInfo(name = "owner_site_admin")
     @SerializedName("site_admin")
-    val siteAdmin: Boolean,
+    val siteAdmin: String? = "",
 
     @ColumnInfo(name = "owner_events_url")
     @SerializedName("events_url")
-    val eventsUrl: String,
+    val eventsUrl: String? = "",
 
     @ColumnInfo(name = "owner_followers_url")
     @SerializedName("followers_url")
-    val followersUrl: String,
+    val followersUrl: String? = "",
 
     @ColumnInfo(name = "owner_following_url")
     @SerializedName("following_url")
-    val followingUrl: String,
+    val followingUrl: String? = "",
 
     @ColumnInfo(name = "owner_gists_url")
     @SerializedName("gists_url")
-    val gistsUrl: String,
+    val gistsUrl: String? = "",
 
     @ColumnInfo(name = "owner_gravatar_id")
     @SerializedName("gravatar_id")
-    val gravatarId: String,
+    val gravatarId: String? = "",
 
     @ColumnInfo(name = "owner_node_id")
     @SerializedName("node_id")
-    val nodeId: String,
+    val nodeId: String? = "",
 
     @ColumnInfo(name = "owner_organizations_url")
     @SerializedName("organizations_url")
-    val organizationsUrl: String,
+    val organizationsUrl: String? = "",
 
     @ColumnInfo(name = "owner_received_events_url")
     @SerializedName("received_events_url")
-    val receivedEventsUrl: String,
+    val receivedEventsUrl: String? = "",
 
     @ColumnInfo(name = "owner_starred_url")
     @SerializedName("starred_url")
-    val starredUrl: String,
+    val starredUrl: String? = "",
 
     @ColumnInfo(name = "owner_subscriptions_url")
     @SerializedName("subscriptions_url")
-    val subscriptionsUrl: String
+    val subscriptionsUrl: String? = ""
 
 )
