6498662e62e3f7962520078ec3dec3a871ff880f
msheraznadeem <sheraz_nadeem04@yahoo.com>
2019-02-18 06:31:32 +0500

    HomeViewModel now interacts with AppRepository to fetch and listen to LiveData events. This is currently not instantiable, will be working on Dependency Injection to make it work with ViewModelFactoryProvider custom implementation.

--- list_repos/app/src/main/java/com/sheraz/listrepos/ui/modules/home/HomeViewModel.kt
+++ list_repos/app/src/main/java/com/sheraz/listrepos/ui/modules/home/HomeViewModel.kt
@@ -1,25 +1,44 @@
 package com.sheraz.listrepos.ui.modules.home
 
 import androidx.databinding.ObservableInt
+import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
+import com.sheraz.listrepos.data.db.entity.GitHubRepoEntity
+import com.sheraz.listrepos.data.repository.AppRepository
 import com.sheraz.listrepos.ui.modules.base.BaseViewModel
 import com.sheraz.listrepos.utils.Logger
 
-class HomeViewModel: BaseViewModel() {
+class HomeViewModel(
+    private val appRepository: AppRepository
+) : BaseViewModel() {
 
     val totalItemsCount = ObservableInt(0)
+
+    private var githubRepoLiveData: LiveData<PagedList<GitHubRepoEntity>>
 
     init {
         Logger.d(TAG, "init(): ")
         setIsLoading(false)
+
+        githubRepoLiveData = appRepository.gitHubRepoEntityPagedListBuilder.build()
     }
 
-    fun fetchRepos() {
+    fun fetchGitHubReposFromNetworkByPage(page: Int) {
+
+        Logger.d(TAG, "fetchGitHubReposFromNetworkByPage(): page: $page")
         setIsLoading(true)
+        appRepository.fetchGitHubReposFromNetwork(page)
+
     }
 
+    fun getLiveData(): LiveData<PagedList<GitHubRepoEntity>> = githubRepoLiveData
 
     override fun onCleared() {
+
+        Logger.d(TAG, "onCleared(): ")
         super.onCleared()
+        appRepository.cancelAllRequests()
+
     }
 
     fun setTotalItemsCount(count: Int) {
