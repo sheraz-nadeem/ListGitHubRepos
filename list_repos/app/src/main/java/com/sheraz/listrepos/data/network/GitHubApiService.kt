b7c0cbec33e237240d5d291d3088b5cd035966a4
msheraznadeem <sheraz_nadeem04@yahoo.com>
2019-02-25 22:55:15 +0500

    Removed unnecessary code from GitHubApiService, now API service's instance is created using Dagger 2 injection. Also now "repos" API method now returns Deferred<Response<T>>.

--- list_repos/app/src/main/java/com/sheraz/listrepos/data/network/GitHubApiService.kt
+++ list_repos/app/src/main/java/com/sheraz/listrepos/data/network/GitHubApiService.kt
@@ -1,11 +1,8 @@
 package com.sheraz.listrepos.data.network
 
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.sheraz.listrepos.data.db.entity.GitHubRepoEntity
 import kotlinx.coroutines.Deferred
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,35 +12,6 @@ interface GitHubApiService {
     fun getReposWithPageAsync(
         @Query("page") page: Int,
         @Query("per_page") per_page: Int
-    ) : Deferred<List<GitHubRepoEntity>>
+    ) : Deferred<Response<List<GitHubRepoEntity>>>
 
-    companion object {
-
-        operator fun invoke(): GitHubApiService {
-
-            // Just to show off that Authorization header can be added
-            // using an Interceptor
-//            val requestInterceptor = Interceptor { chain ->
-//
-//                val original = chain.request()
-//                val newRequest = original.newBuilder()
-//                    .addHeader("Authorization", "AUTH_TOKEN")
-//                    .build()
-//
-//                return@Interceptor chain.proceed(newRequest)
-//            }
-
-            val okHttpClient = OkHttpClient.Builder()
-//                .addInterceptor(requestInterceptor)
-                .build()
-
-            return Retrofit.Builder()
-                .client(okHttpClient)
-                .baseUrl("https://api.github.com/users/xing/")
-                .addCallAdapterFactory(CoroutineCallAdapterFactory())
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(GitHubApiService::class.java)
-        }
-    }
 }
