24b93fe83088dbadea4f59925b2d10e25c401f5e
msheraznadeem <sheraz_nadeem04@yahoo.com>
2019-03-03 16:47:19 +0500

    Added "safeApiCall" extension function to give us flexibility of try and catch without writing too many try-catch statements everywhere, instead use this extension and pass networkBlock, failureBlock and error message and be done with it.

--- list_repos/app/src/main/java/com/sheraz/listrepos/internal/Extensions.kt
+++ list_repos/app/src/main/java/com/sheraz/listrepos/internal/Extensions.kt
@@ -3,6 +3,8 @@ package com.sheraz.listrepos.internal
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.sheraz.listrepos.utils.Logger
+import java.io.IOException
 
 
 /**
@@ -17,4 +19,19 @@ inline fun <reified T : Fragment> AppCompatActivity.findFragmentByTagWithAutoDis
         is DialogFragment -> fragment.dismiss()
     }
     return fragment
+}
+
+
+
+/**
+ * Network Utility Extension functions  
+ */
+suspend fun safeApiCall(networkBlock: suspend () -> Unit, failureBlock: (Exception) -> Unit, errorMessage: String) {
+    return try {
+        networkBlock()
+    } catch (e: Exception) {
+        // An exception was thrown when calling the API so we're converting this to an IOException
+        Logger.e("safeApiCall", "safeApiCall(): Exception occurred, Error => " + e.message)
+        failureBlock(IOException(errorMessage, e))
+    }
 }
